9acb54f8f241400b4f9a2bd58104df65c77055e8
SeltzerJon <bestfriends90@gmail.com>
2024-02-07 18:19:14 -0500

    Second Update

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const User = require('./models/users');
+const bcrypt = require('bcrypt');
 
 const app = express();
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));;
 
-mongoose.connect('', { useNewUrlParser: true, useUnifiedTopology: true });
+// K8gFPXYMjDjbKw5u
+mongoose.connect('');
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/signup.html');
@@ -27,5 +28,39 @@ app.post('/signup', (req, res) => {
         } else {
             res.send('Signed up successfully!');
         }
+    });
+
+app.post('/login', async (req, res) => {
+        const { username, password } = req.body;
+      
+        try {
+          const user = await User.findOne({ username });
+          if (!user) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+          }
+      
+        //   Hash passwords securely
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          if (!passwordMatch) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+          }
+      
+          req.session.user = user;
+          res.status(200).json({ message: 'Login successful' });
+        } catch (error) {
+          console.error('Error:', error);
+          res.status(500).json({ error: 'Internal server error' });
+        }
+      });
+      
+app.post('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+        console.error('Error:', err);
+        res.status(500).json({ error: 'Internal server error' });
+        } else {
+        res.status(200).json({ message: 'Logout successful' });
+        }
+    });
     });
 });
